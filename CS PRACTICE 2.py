4bd2fd4d0cfc2e79ca37a184c2fd4a3e29eaee02
Ayshee <152168245+Ayshee782@users.noreply.github.com>
2025-02-26 20:43:30 +0600

    Add files via upload

---
+++ CS PRACTICE 2.py
@@ -0,0 +1,10 @@
+"practice 1 using in logical exprission"
+a=int(input("enter marks= "))
+if((a>=100)or(a>=90)):
+    print("A")
+elif((a>=90)or(a>=80)):
+    print("B")
+elif((a>=80)or(a>=70)):
+    print("C")
+else:
+    print("D")
