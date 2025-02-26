6351379363edc24b98b14f40d767d65842e5e25b
Ayshee <152168245+Ayshee782@users.noreply.github.com>
2025-02-26 21:39:31 +0600

    Add files via upload

---
+++ CS PRACTICE 4.py
@@ -0,0 +1,12 @@
+#integer input and prints "Even" if it's even and "Odd" if it's odd.
+
+#solution 1
+a=int(input("enter number = "))
+
+if(a%2==0):
+    print("even")
+else:
+    print("odd")
+
+
+
