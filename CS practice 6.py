16da3a7e519a4bc54bb6d3afcd6a6d9d53050a51
Ayshee <152168245+Ayshee782@users.noreply.github.com>
2025-02-26 22:54:46 +0600

    Add files via upload

---
+++ CS practice 6.py
@@ -0,0 +1,6 @@
+#asks for the user's age and prints "Adult" if the age is 18 or above, otherwise prints "Minor".
+age=int(input("enter the age = "))
+if(age>=18):
+    print("Adult")
+else:
+    print("Minor")
