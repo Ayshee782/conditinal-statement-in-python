fabf56655f7912d7be20d76337ad503a0de39c31
Ayshee <152168245+Ayshee782@users.noreply.github.com>
2025-02-26 21:00:24 +0600

    Add files via upload

---
+++ CS PRACTICE 3.py
@@ -0,0 +1,8 @@
+#checks if a number is positive, negative, or zero.
+a=int(input("enter num= "))
+if(a<0):
+    print("It's negative")
+elif(a>0):
+    print("it's positive")
+else:
+    print("zero")
