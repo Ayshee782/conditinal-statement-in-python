819157d1f4c814c631d92c1c8d0b25ca2abbe89a
Ayshee <152168245+Ayshee782@users.noreply.github.com>
2025-05-02 22:08:17 +0600

    Add files via upload

---
+++ greatest of two numbers.py
@@ -0,0 +1,13 @@
+#Write a program to find the greatest of two numbers
+
+a= (input ("num 1= "))
+print(a)
+b=(input("num 2= "))
+print(b)
+
+if (b>=a):
+    print ("hmm")
+elif(a>=b):
+    print("noo")
+else:
+    print("eww")
